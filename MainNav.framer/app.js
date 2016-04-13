c510c5f4eba0e0b2654d33b430b771aa9915f671
Jesse Rivera <jrivera@jrivera-iMac.sccadv.com>
2016-04-13 16:59:12 -0500

    Initial Commit

---
+++ MainNav.framer/app.js
@@ -0,0 +1,186 @@
+myLayers = Framer.Importer.load("imported/MainNav@1x")
+
+var scroll = new ScrollComponent({
+	width: 1200,
+	height: 400,
+	scrollHorizontal: false
+});
+
+scroll.onScroll( function() {
+	if(scroll.scrollY > 50) {
+		minNav();
+	} else {
+		maxNav();
+	}
+});
+
+for (layerName in myLayers) {
+	var layer = myLayers[layerName]
+
+	layer.superLayer = scroll.content;
+}
+
+/*
+==============================================================
+	Hide small components
+==============================================================
+*/
+
+myLayers['SmallNavBar'].y = -50;
+myLayers['SmallLogo'].y = -50;
+myLayers['MenuIcon'].y = -50;
+
+/*
+==============================================================
+	Min and Max Nav
+==============================================================
+*/
+
+var maxNav = function() {
+	showLogo();
+	showLinks();
+	hideSmallMenu();
+	hideSmallLogo();
+	hideMenuIcon();
+}
+
+var minNav = function() {
+	hideLogo();
+	hideLinks();
+	showSmallMenu();
+	showSmallLogo();
+	showMenuIcon();
+}
+
+
+/*
+==============================================================
+	Show and Hide Logo
+==============================================================
+*/
+var hideLogo = function() {
+	myLayers['Logo'].animate({
+		properties: {
+			x: -400
+		},
+		time: .02
+	});
+}
+
+var showLogo = function() {
+	myLayers['Logo'].animate({
+		properties: {
+			x: 10
+		},
+		time: .2
+	});
+}
+
+
+/*
+==============================================================
+	Show and Hide Small Menu
+==============================================================
+*/
+var hideSmallMenu = function() {
+	myLayers['SmallNavBar'].animate({
+		properties: {
+			y: -100
+		},
+		time: .2
+	});
+}
+
+var showSmallMenu = function() {
+	myLayers['SmallNavBar'].animate({
+		properties: {
+			y: scroll.scrollY
+		},
+		time: .2
+	});
+}
+
+/*
+==============================================================
+	Show and Hide Small Logo
+==============================================================
+*/
+var hideSmallLogo = function() {
+	myLayers['SmallLogo'].animate({
+		properties: {
+			y: -100
+		},
+		time: .02
+	});
+}
+
+var showSmallLogo = function() {
+	myLayers['SmallLogo'].animate({
+		properties: {
+			y: scroll.scrollY
+		},
+		time: .3
+	});
+}
+
+/*
+==============================================================
+	Show and Hide Menu Icon
+==============================================================
+*/
+var hideMenuIcon = function() {
+	myLayers['MenuIcon'].animate({
+		properties: {
+			y: scroll.scrollY+10,
+			x: 1300
+		},
+		time: .02
+	});
+}
+
+var showMenuIcon = function() {
+	myLayers['MenuIcon'].animate({
+		properties: {
+			y: scroll.scrollY+10,
+			x: 1150
+		},
+		time: .2
+	});
+}
+
+/*
+==============================================================
+	1. Show and Hide Links
+	2. Animate Links
+==============================================================
+*/
+
+
+//	1. Show hide Links
+
+
+var showLinks = function() {
+	var w = 188;
+	animateLink('Nav1', 450);
+	animateLink('Nav2', 450 + (w*1));
+	animateLink('Nav3', 450 + (w*2));
+	animateLink('Nav4', 450 + (w*3));
+}
+
+var hideLinks = function() {
+	animateLink('Nav1', 1700);
+	animateLink('Nav2', 1700);
+	animateLink('Nav3', 1700);
+	animateLink('Nav4', 1700);
+}
+
+//	2. Animate Links
+
+var animateLink = function(layer, pos) {
+	myLayers[layer].animate({
+		properties: {
+			x: pos
+		},
+		time: .5
+	})
+}
---
+++ MainNav.framer/imported/MainNav@1x/layers.json.js
@@ -0,0 +1,228 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["MainNav@1x/layers.json.js"] = [
+	{
+		"id": 25,
+		"name": "Content",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Content.png",
+			"frame": {
+				"x": 15,
+				"y": 140,
+				"width": 1174,
+				"height": 342
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "258429824"
+	},
+	{
+		"id": 29,
+		"name": "SmallNavBar",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/SmallNavBar.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 1200,
+				"height": 50
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "360187449"
+	},
+	{
+		"id": 33,
+		"name": "MenuIcon",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/MenuIcon.png",
+			"frame": {
+				"x": 1152,
+				"y": 12,
+				"width": 33,
+				"height": 27
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1161284258"
+	},
+	{
+		"id": 35,
+		"name": "SmallLogo",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/SmallLogo.png",
+			"frame": {
+				"x": 4,
+				"y": 4,
+				"width": 148,
+				"height": 42
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1334264099"
+	},
+	{
+		"id": 6,
+		"name": "Nav4",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Nav4.png",
+			"frame": {
+				"x": 1010,
+				"y": 10,
+				"width": 179,
+				"height": 79
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1490926460"
+	},
+	{
+		"id": 13,
+		"name": "Nav3",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Nav3.png",
+			"frame": {
+				"x": 824,
+				"y": 10,
+				"width": 179,
+				"height": 79
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "748601913"
+	},
+	{
+		"id": 17,
+		"name": "Nav2",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Nav2.png",
+			"frame": {
+				"x": 638,
+				"y": 10,
+				"width": 179,
+				"height": 79
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "281984325"
+	},
+	{
+		"id": 21,
+		"name": "Nav1",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Nav1.png",
+			"frame": {
+				"x": 452,
+				"y": 10,
+				"width": 179,
+				"height": 79
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1708039624"
+	},
+	{
+		"id": 2,
+		"name": "Logo",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1200,
+			"height": 500
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Logo.png",
+			"frame": {
+				"x": 8,
+				"y": 8,
+				"width": 210,
+				"height": 81
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "570622248"
+	}
+]
